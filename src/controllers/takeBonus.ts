124ab09f21e65445f7f1837ab867295dc558ed5e
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-12-02 18:25:21 +0200

    feat: add redirection to casino site

--- src/controllers/takeBonus.ts
+++ src/controllers/takeBonus.ts
@@ -2,6 +2,7 @@ import { enableButton } from '../utils/enableButton';
 import { hideModal } from './hideModal';
 
 export function takeBonus() {
+  window.open('https://trinocasino.com/', '_blank');
   enableButton('spinWheelButton');
   hideModal();
 }
